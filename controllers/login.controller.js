859c331a0267bbf7f47864530498b80cdc2906fc
Yuri <dr.dampel@yandex.ru>
2023-02-04 00:16:09 +0300

    Added hash password.

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -1,4 +1,5 @@
 const db = require('../db');
+const crypto = require('crypto');
 
 class LoginController {
 
@@ -7,11 +8,12 @@ class LoginController {
 
         try {
             const { login, password } = request.body;
+            const hash = crypto.createHash('sha256').update(password).digest('hex');
 
             const account = await db.query(`
             select users.id, users."name", users.surname, users.patronymic, users.age, users.snpassport, users.phone, users.photo, users.email, users.id_login, users.id_role, logins.login, logins."password", roles.title 
             from logins join users on logins.id = users.id_login join roles on users.id_role = roles.id 
-            where login = '${login}' and "password" = '${password}'
+            where login = '${login}' and "password" = '${hash}'
             `);
 
             if (account.rows[0] == undefined) {
@@ -31,6 +33,7 @@ class LoginController {
                 const history = await db.query(`select rents.id, title, start_date, end_date, total_price from rents join cars on rents.id_car = cars.id where id_user=${account.rows[0].id}`);
                 response.json({ status: 200, value: account.rows[0], history: history.rows });
             }
+
         }
         catch (error) {
             console.log(error);
--- controllers/registr.controller.js
+++ controllers/registr.controller.js
@@ -1,13 +1,16 @@
 const db = require('../db');
+const crypto = require('crypto');
 
 class RegistController {
 
     //POST запрос.
     async registration(request, response) {
-        const { name, surname, patronymic, snpassport, age, phone, email, login, password } = request.body;
-
         try {
-            const _login = await db.query(`SELECT * FROM logins WHERE login='${login}' and password='${password}'`);
+            const { name, surname, patronymic, snpassport, age, phone, email, login, password } = request.body;
+
+        const hash = crypto.createHash('sha256').update(password).digest('hex');
+
+        const _login = await db.query(`SELECT * FROM logins WHERE login='${login}' and password='${hash}'`);
             if(_login.rows.length > 0){
                 response.status(400);
                 response.send("Exist login");
@@ -25,7 +28,7 @@ class RegistController {
                     do $$
                         declare idLogin INTEGER;
                         begin
-                        INSERT INTO logins (login, "password") VALUES('${login}', '${password}');
+                        INSERT INTO logins (login, "password") VALUES('${login}', '${hash}');
                         select (select id from logins where login = '${login}') into idLogin;
 
                         INSERT INTO users ("name", surname, patronymic, age, snpassport, phone, photo, email, id_login, id_role)
@@ -37,12 +40,14 @@ class RegistController {
 
             response.status(200);
             response.send("Success");
-        }
+        } 
         catch (error) {
             console.log(error);
             response.status(400);
             response.send("Bad");
         }
+        
+
     }
 }
 
