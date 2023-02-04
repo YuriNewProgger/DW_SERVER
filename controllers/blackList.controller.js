63e41b6b4473db7e1219585ab6263e612c208172
Yuri <dr.dampel@yandex.ru>
2023-02-04 16:47:29 +0300

    Make method add user to black list.

--- controllers/blackList.controller.js
+++ controllers/blackList.controller.js
@@ -1,19 +1,54 @@
 const db = require('../db');
 
-class BlackList{
+class BlackList {
 
-    async getBlackList(request, response){
+    async getBlackList(request, response) {
 
-        
+
         try {
             const list = await db.query(`select users.name, users.surname , users.snpassport , users.id as "user_id", black_list.id as "blacklist_id", black_list.reason 
             from users join black_list on users.id = black_list.id_user `);
 
-            response.json({status: 200, values: list.rows});
-        } 
+            response.json({ status: 200, values: list.rows });
+        }
         catch (error) {
             console.log(error);
-            response.json({status: 500, values: []});
+            response.json({ status: 500, values: [] });
+        }
+    }
+
+    async addUserToBlackList(request, response) {
+        try {
+            const { snpassport, reason } = request.body;
+
+            const user = await db.query(`select id from users where snpassport = '${snpassport}'`);
+
+            if (user.rows.length === 0) {
+                response.json({ status: 404, values: [] })
+                return;
+            }
+
+            await db.query(`INSERT INTO black_list (id_user, reason) VALUES(${user.rows[0].id}, '${reason}');`);
+
+            const list = await db.query(`select users.name, users.surname , users.snpassport , users.id as "user_id", black_list.id as "blacklist_id", black_list.reason 
+            from users join black_list on users.id = black_list.id_user `);
+
+            response.json({ status: 200, values: list.rows });
+
+        }
+        catch (error) {
+            response.sendStatus(500);
+            console.log(error);
+        }
+
+    }
+
+    async deleteFromBlackList(request, response){
+        try {
+            //await db.query()
+        } 
+        catch (error) {
+            
         }
     }
 }
--- routes/blacklist.routers.js
+++ routes/blacklist.routers.js
@@ -3,5 +3,6 @@ const router = new Router();
 const blc = require('../controllers/blackList.controller');
 
 router.get('/blackList', blc.getBlackList);
+router.post('/addUser', blc.addUserToBlackList);
 
 module.exports = router;
