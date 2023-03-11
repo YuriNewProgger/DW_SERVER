4c98470c9cf85d68f8b4947181d2ce62d8522c53
Yuri <dr.dampel@yandex.ru>
2023-03-11 11:43:46 +0300

    Refacoring

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 const Pool = require('pg').Pool;
 const pool = new Pool(
     {
-        user: 'postgres',
-        password: '123',
-        host: 'localhost',
-        port: 5432,
-        database: 'SAA'
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        host: process.env.HOST,
+        port: process.env.PORT,
+        database: process.env.DATABASE
     }
 );
 
